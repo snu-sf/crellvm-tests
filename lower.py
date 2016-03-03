0a653ba24ed8d836eb76e932b14fdc17aca9fecd
SungMinCho <sungmincho@SungMinChos-MacBook-Pro.local>
2016-03-03 19:46:44 +0900

    fixed issues

--- lower.py
+++ lower.py
@@ -24,9 +24,11 @@ if __name__ == "__main__":
      
     optpath = arg_results.exepath
     inputpath = arg_results.inputpath
+    # if user said "input/", he probably meant "input"... i guess
     if inputpath[-1] == '/':
       inputpath = inputpath[:-1]
-    resultpath = inputpath + "_lower"
+    resultpath = os.path.join(os.path.dirname(inputpath),
+                              os.path.basename(inputpath) + "_lower")
 
     if os.path.exists(resultpath):
       logger.error("{0} path already exists! please erase the directory".format(resultpath))
@@ -34,28 +36,26 @@ if __name__ == "__main__":
 
     os.mkdir(resultpath)
 
-    if not os.path.exists(optpath) :
+    if not os.path.exists(optpath):
         logger.error("{0} file does not exist".format(optpath))
         sys.exit(1)
     if not os.path.exists(inputpath):
         logger.error("{0} folder does not exist".format(inputpath))
         sys.exit(1)
 
-    # for each subfolder in "inputs"..
     for testname in os.listdir(inputpath) : 
         testdir = os.path.join(inputpath, testname)
-        # create new sub directory in results-opt folder.. 
         test_outputdir = os.path.join(resultpath, testname)
         os.mkdir(test_outputdir)
 
-        for bitcode_file in os.listdir(testdir) : 
-            if not bitcode_file.endswith(".ll"):
+        for ll_file in os.listdir(testdir) : 
+            if not ll_file.endswith(".ll"):
                 continue
 
-            bitcode_path = os.path.join(testdir, bitcode_file)
-            bitcode_outputpath = os.path.join(test_outputdir, bitcode_file)
+            ll_path = os.path.join(testdir, ll_file)
+            ll_outputpath = os.path.join(test_outputdir, ll_file)
             
-            retcode = subprocess.call([optpath, "-lowerswitch", "-S", "-o", bitcode_outputpath, bitcode_path])
+            retcode = subprocess.call([optpath, "-lowerswitch", "-S", "-o", ll_outputpath, ll_path])
             if retcode <> 0:
               logger.error("{0} halted with non-zero return value!".format(optpath))
               sys.exit(1)
