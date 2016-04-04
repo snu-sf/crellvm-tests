05eab60887e077107ed247e98735a1da3411f9b8
alxest <youngju.song@sf.snu.ac.kr>
2016-04-04 20:13:07 +0900

    Update vali.rb

--- vali.rb
+++ vali.rb
@@ -1,7 +1,8 @@
 raise "Argument # should be >= 1, but it is #{ARGV.length}" unless ARGV.length >= 1
 
 $name = ARGV[0]
-OPT_OPTION = ARGV[1].nil?? "-basicaa -gvn" : ARGV[1]
+# OPT_OPTION = ARGV[1].nil?? "-basicaa -gvn" : ARGV[1]
+OPT_OPTION = ARGV[1].nil?? "-instcombine" : ARGV[1]
 OUT_NAME = "output"
 CLEAN_TRIPLE_BEFORE = true
 
@@ -20,8 +21,6 @@ def make
   Dir.chdir("#{cur}")
 end
 
-make
-
 def change_to_bc(name)
   ext = name.split(".").last #File.extname(name)
   base = name.split(".")[0...-1].join(".") #File.basename(name, ext)
@@ -36,48 +35,75 @@ def change_to_bc(name)
     run("llvm-as #{base}.ll")
     # run("clang++ #{base}.cpp -emit-llvm -c -o #{base}.bc")
   when "bc"
-    run("llvm-dis #{base}.bc")
+    # run("llvm-dis #{base}.bc")
+    # we edit ll file, then bc is generated,
+    # and next time our original ll file is overwritten and can
+    # never update it
+    run("llvm-dis #{base}.bc") unless File.exists?("#{base}.ll")
   when "ll"
     run("llvm-as #{base}.ll")
   else
-    raise "No file extension matched"
+    raise "No file extension matched : #{name}"
   end
   base
 end
 
 def classify(name)
   ns = name.split(".")
-  #2 : *.ll or *.bc or *.cpp or *.c or blah
-  return 0 if ns.size == 2 && "c cpp bc ll".split.include?(ns.last)
-  #3 : hint triple (*.src.bc, *.tgt.bc, *.hint.json)
-  # do not check ns.size, AndOrXor.pow2.0.src.bc
+  # t = "hint.json src.bc tgt.bc".split.include?(ns[-2..-1].join("."))
+  # puts "name : #{name}, ns : #{ns[-2..-1].join(""".""")} : #{t}"
+  #for validate -> hint triple (*.src.bc, *.tgt.bc, *.hint.json)
   return 1 if "hint.json src.bc tgt.bc".split.include?(ns[-2..-1].join("."))
+  #for generate -> *.ll or *.bc or *.cpp or *.c
+  return 0 if "c cpp bc ll".split.include?(ns.last)
   return -1 if "src.ll tgt.ll #{OUT_NAME}.ll".split.include?(ns[-2..-1].join("."))
   raise "not cared name : #{name}"
 end
 
+# def classify(name)
+#   ns = name.split(".")
+#   #2 : *.ll or *.bc or *.cpp or *.c or blah
+#   return 0 if ns.size == 2 && "c cpp bc ll".split.include?(ns.last)
+#   #3 : hint triple (*.src.bc, *.tgt.bc, *.hint.json)
+#   # do not check ns.size, AndOrXor.pow2.0.src.bc
+#   return 1 if "hint.json src.bc tgt.bc".split.include?(ns[-2..-1].join("."))
+#   return -1 if "src.ll tgt.ll #{OUT_NAME}.ll".split.include?(ns[-2..-1].join("."))
+#   raise "not cared name : #{name}"
+# end
+
 
 def generate(name)
-  puts "-----------generate start-------------"
+  # puts "-----------generate start-------------"
   base = change_to_bc name
   puts "#{name} #{base}"
-  result = run("opt #{OPT_OPTION} #{base}.ll -o #{base}.#{OUT_NAME}.ll")
-  puts result
-  puts "-----------generate end---------------"
+  result = run("opt #{OPT_OPTION} #{base}.ll -o #{base}.#{OUT_NAME}.ll -S")
+  # puts result
+  # puts "-----------generate end---------------"
 end
 
 $failed = 0
 $success = 0
 $not_supported = 0
-$aborted = 0
+$validation_failed = 0
+$other_fail = []
+
+def write_stat(result)
+  ($success += 1; return) if $?.success?
+  $failed += 1
+  puts result
+  puts "#{result["Not_Supported"]}"
+  if result["Not_Supported"] then $not_supported += 1
+  else
+    if result["Validation failed."] then $validation_failed += 1
+    else $other_fail += [result] end
+  end
+end
 
 def validate(hint, src, tgt)
   run("llvm-dis #{src}")
   run("llvm-dis #{tgt}")
-  result = %x(../ocaml_refact/main.native #{src} #{tgt} #{hint} 2>&1)
-  $?.success?? $success += 1 : $failed += 1
-  $not_supported += 1 if result["Not_Supported"]
-  $aborted += 1 if result["Aborted"]
+  result = %x(zsh -c "../ocaml_refact/main.native #{src} #{tgt} #{hint} 2>&1")
+  write_stat(result)
 end
 
 def clean_triple
@@ -88,11 +114,17 @@ def clean_triple
   Dir.chdir("#{cur}")
 end
 
+make
+
 if File.directory?($name)
   #why not "each"? it is parallized and outputs are mixed.
   clean_triple if CLEAN_TRIPLE_BEFORE
-  Dir["#{$name}/*"].select{|i| (classify i) == 0}.uniq{|n| n.split(".")[0]}.map{|n| generate n}
-  h = Dir["#{$name}/*"].select{|i| (classify i) == 1}.group_by{|n| n.split(".")[0..2].join(".")}
+  # Dir["#{$name}/*"].select{|i| (classify i) == 0}.uniq{|n| n.split(".")[0]}.map{|n| generate n}
+  # h = Dir["#{$name}/*"].select{|i| (classify i) == 1}.group_by{|n| n.split(".")[0..2].join(".")}
+  files = Dir["#{$name}/**/*"].reject{|f| File.directory? f}
+  files.select{|i| (classify i) == 0}.uniq{|n| n.split(".")[0]}.map{|n| generate n}
+  h = files.select{|i| (classify i) == 1}.group_by{|n| n.split(".")[0..2].join(".")}
+  puts h
   h.each{|k,v|
     puts "#{k} #{v}";
     raise "not triple : #{v}" if v.size != 3;
@@ -104,7 +136,9 @@ else
 end
 
 puts "total: #{h.size}"
-puts "failed: #{$failed}"
 puts "success: #{$success}"
-puts "not_supported: #{$not_supported}"
-puts "aborted: #{$aborted}"
+puts "failed: #{$failed}"
+puts "not supported: #{$not_supported}"
+puts "validation failed: #{$validation_failed}"
+puts "other fail: #{$other_fail.size}"
+puts "#{$other_fail.join}"
